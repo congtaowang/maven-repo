a8f3498bf3fe62b2606a3578bb60ada579d6d04c
congtaowang <congtaowang225@gmail.com>
2017-04-28 13:27:14 +0800

    Commit.

--- auto-publish.py
+++ auto-publish.py
@@ -8,6 +8,20 @@ import sys
 opts = sys.argv[1:]
 print opts
 
+def descinfo():
+ try:
+  opts = sys.argv[1:]
+  if len(opts) == 0:
+   return 'Commit.'
+  else:
+   if opts.contains('-i'):
+    val = opts[opts.indexOf('-i')+1]
+    if val == None or val == '':
+     return 'Commit.'
+    return val
+ except:
+  return 'Commit.'
+
 os.system("git add .")
-os.system("git commit -m %s" % ("Commit."))
+os.system("git commit -m %s" % (descinfo()))
 os.system("git push -u origin master")
